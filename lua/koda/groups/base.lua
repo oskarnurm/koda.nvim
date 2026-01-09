f1bbccd009e6c3da461cff3a4c784c6380e26cbf
oskarnurm <19738295+oskarnurm@users.noreply.github.com.>
2026-01-09 20:04:17 +0200

    fix(base): remove and replace all instances of `c.bg_solid` with `c.bg`
    `c.bg_solid` was removed in a previous commit, but I forgot to update
    every instance of it. This fixes it now.

--- lua/koda/groups/base.lua
+++ lua/koda/groups/base.lua
@@ -28,7 +28,7 @@ function M.get(c, opts)
       PmenuSel          = { fg = c.bg, bg = c.emphasis, bold = true },
       PmenuThumb        = { bg = c.fg },
       PmenuMatch        = { fg = c.const, bold = true },
-      Visual            = { fg = c.emphasis, bg = utils.blend(c.highlight, c.bg_solid, 0.4) },
+      Visual            = { fg = c.emphasis, bg = utils.blend(c.highlight, c.bg, 0.4) },
       Search            = { link = "Visual" },
       IncSearch         = { link = "Search" },
       CurSearch         = { fg = c.bg, bg = c.const },
@@ -44,10 +44,10 @@ function M.get(c, opts)
       QuickFixLine      = { fg = c.const, underline = true },
       qfLineNr          = { fg = c.dim },
       Title             = { fg = c.emphasis, bold = true },
-      DiffAdd           = { fg = c.success, bg = utils.blend(c.success, c.bg_solid, 0.2) },
-      DiffChange        = { fg = c.warning, bg = utils.blend(c.warning, c.bg_solid, 0.2) },
-      DiffDelete        = { fg = c.danger, bg = utils.blend(c.danger, c.bg_solid, 0.2) },
-      DiffText          = { fg = c.warning, bg = utils.blend(c.warning, c.bg_solid, 0.4) },
+      DiffAdd           = { fg = c.success, bg = utils.blend(c.success, c.bg, 0.2) },
+      DiffChange        = { fg = c.warning, bg = utils.blend(c.warning, c.bg, 0.2) },
+      DiffDelete        = { fg = c.danger, bg = utils.blend(c.danger, c.bg, 0.2) },
+      DiffText          = { fg = c.warning, bg = utils.blend(c.warning, c.bg, 0.4) },
     }
 end
 
